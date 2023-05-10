875d6c382947c72c5adbe4161c895aa0408dc330
eden lane <edencore@gmail.com>
2023-05-10 13:37:20 +0300

    feat: saving selected product id

--- packages/client/src/pages/Main/MainPage.tsx
+++ packages/client/src/pages/Main/MainPage.tsx
@@ -1,18 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AllProductsChart } from '../../features/products/AllProductsChart';
 import { ProductsSelector } from '../../features/products/ProductsSelector';
 import { useProductsQuery } from '../../features/products/products.api';
 
+const SELECTED_PRODUCT_OPTION_KEY = 'ugmk:selectedProduct';
+
 export const MainPage = () => {
   const { data: products } = useProductsQuery();
-  const [selectedProduct, setSelectedProduct] = useState('all');
+  const [selectedProduct, setSelectedProduct] = useState<string>(() => {
+    const productId = localStorage.getItem(SELECTED_PRODUCT_OPTION_KEY);
+    return productId || 'all';
+  });
+
+  const handleChangeProduct = (productId: string) => {
+    localStorage.setItem(SELECTED_PRODUCT_OPTION_KEY, productId);
+    setSelectedProduct(productId);
+  }
 
   return (
     <>
       <ProductsSelector
         data={products}
         value={selectedProduct}
-        onChange={setSelectedProduct}
+        onChange={handleChangeProduct}
       />
       <AllProductsChart data={products} selectedProduct={selectedProduct} />
     </>
